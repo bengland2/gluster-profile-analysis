f25c01522bc4f3ae75b24a9c42dc44583d7acebf
Ben England <bengland@redhat.com>
2015-11-10 13:33:25 -0500

    client cant do gluster commands, let user specify log, dont need volume info

--- gvp-client.sh
+++ gvp-client.sh
@@ -2,16 +2,19 @@
 # gvp-client.sh - collect perf data from Gluster for client's usage 
 # of Gluster volume from 1 mountpoint
 #
+# ASSUMPTION: "gluster volume profile your-volume start" has already happened
+#
 # usage: 
 #  chmod u+x gvp-client.sh
-#  ./gvp-client.sh your-gluster-volume your-client-mountpoint samples interval
+#  ./gvp-client.sh your-gluster-volume your-client-mountpoint samples interval [ logfile ]
 #
 volume_name=$1
 mountpoint=$2
 sample_count=$3
 sample_interval=$4
+your_logfile=$5
 if [ "$sample_interval" = "" ]  ; then
-  echo "usage: gvp-client.sh your-gluster-volume your-client-mountpoint sample-count sample-interval-sec"
+  echo "usage: gvp-client.sh your-gluster-volume your-client-mountpoint sample-count sample-interval-sec [ your-logfile ]"
   exit 1
 fi
 
@@ -19,15 +22,12 @@ sample_cmd="setfattr -n trusted.io-stats-dump -v "
 
 timestamp=`date +%Y-%m-%d-%H-%M`
 logfile=/var/tmp/gvp-client-${timestamp}.log
+if [ -n "$your_logfile" ] ; then
+  logfile="$your_logfile"
+fi
 
 # make sure not polluted with previous data
 rm -f $logfile
-
-# capture configuration
-gluster volume info > /var/tmp/gvi-${timestamp}.log
-
-# enable iostats translator
-gluster volume profile $volume_name start
 
 # so next sample interval will be $sample_interval
 $sample_cmd /var/tmp/gvp.log $mountpoint
@@ -36,8 +36,7 @@ for min in `seq 1 $sample_count` ; do
   sleep $sample_interval
   rm -f /var/tmp/gvp.log
   $sample_cmd /var/tmp/gvp.log $mountpoint
-  ( date ; cat /var/tmp/gvp.log ) >> /var/tmp/gvp-client-${timestamp}.log
+  ( date ; cat /var/tmp/gvp.log ) >> $logfile
 done
-gluster volume profile $volume_name stop
-echo "output written to /var/tmp/gv*$timestamp.log with timestamp $timestamp"
+echo "output written to $logfile"
 
